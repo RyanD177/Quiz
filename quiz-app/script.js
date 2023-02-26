23cdc874dd1a765885aff5e8154fa6e698bb1111
RyanD177 <w0477770@campus.nscc.ca>
2023-02-26 16:37:34 -0400

    Data made

---
+++ quiz-app/script.js
@@ -0,0 +1,49 @@
+const quizData = [
+    {
+        question: 'How old is Ryan',
+        a: '10',
+        b: '25',
+        c: '110',
+        d: 'c',
+        correct: "b"
+    }, {
+        question: 'What is the best Programming language',
+        a: 'Java',
+        b: 'JavaScript',
+        c: 'PHP',
+        d: 'C++',
+        correct: "b"
+    }, {
+        question: 'What is the name of my dog',
+        a: 'Dollar',
+        b: 'Jimmy',
+        c: 'Timmy',
+        d: 'Rimmy',
+        correct: "a"
+    },
+    {
+        question: 'Whats my favorite Color',
+        a: 'Blue',
+        b: 'Red',
+        c: 'Orange',
+        d: 'Green',
+        correct: "a"
+    }, 
+        {
+            question: 'When was Javascipt made',
+            a: 'Dollar',
+            b: 'Jimmy',
+            c: 'Timmy',
+            d: 'Rimmy',
+            correct: "a"
+        }
+
+
+
+    
+
+
+
+]
+
+
